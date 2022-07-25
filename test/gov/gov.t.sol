aaaa71bc99f910499e1c306980aaa9cf6836feed
horsefacts <webmaster@terminally.online>
2022-07-25 00:40:30 -0400

    More gov tests

--- test/gov/gov.t.sol
+++ test/gov/gov.t.sol
@@ -24,6 +24,7 @@ import {CTR} from "../../src/gov/ctr.sol";
 import {DSToken} from "ds-token/token.sol";
 import {DSChief, DSChiefFab} from "ds-chief/chief.sol";
 import {DSPause, DSPauseProxy} from "ds-pause/pause.sol";
+import {DSSpell, DSSpellBook} from "ds-spell/spell.sol";
 
 interface VmLike {
     function prank(address) external;
@@ -47,6 +48,8 @@ contract TestGov is DSTest {
     DSChief internal chief;
     DSPause internal pause;
     DSPauseProxy internal pauseProxy;
+    DSSpellBook internal spellBook;
+    DSSpell internal spell;
 
     address me = address(this);
 
@@ -69,6 +72,23 @@ contract TestGov is DSTest {
 
         pause = new DSPause(172800, address(0), address(chief));
         pauseProxy = pause.proxy();
+
+        spellBook = new DSSpellBook();
+
+        // Assemble the plot
+        address      usr = address(sum);
+        bytes32      tag;  assembly { tag := extcodehash(usr) }
+        bytes memory fax = abi.encodeWithSignature("file(bytes32,uint)", "One", 2);
+        uint256      eta = now + pause.delay();
+
+        bytes memory plot = abi.encodeWithSignature(
+            "plot(address,bytes32,bytes memory,uint)",
+            usr,
+            tag,
+            fax,
+            eta
+        );
+        spell = spellBook.make(address(pause), 0, plot);
     }
 
     function test_governance_deployment() public {
@@ -113,14 +133,69 @@ contract TestGov is DSTest {
         // Deployer address holds full CTR supply
         assertEq(ctr.balanceOf(me), ctr.totalSupply());
 
-        // Transfer to Alice
-        ctr.push(alice, 100_000);
+        // Transfer to voters
+        ctr.push(alice, 100_000 ether);
+        ctr.push(bob,   200_000 ether);
+        ctr.push(carol, 300_000 ether);
 
-        // Transfer to Bob
-        ctr.push(alice, 200_000);
+        // Approvals to Chief
+        vm.prank(alice);
+        ctr.approve(address(chief), 100_000 ether);
 
-        // Transfer to Carol
-        ctr.push(alice, 300_000);
+        vm.prank(bob);
+        ctr.approve(address(chief), 200_000 ether);
+
+        vm.prank(carol);
+        ctr.approve(address(chief), 300_000 ether);
+
+        // Alice locks CTR
+        vm.prank(alice);
+        chief.lock(100_000 ether);
+
+        // Bob locks CTR
+        vm.prank(bob);
+        chief.lock(200_000 ether);
+
+        // Carol locks CTR
+        vm.prank(carol);
+        chief.lock(300_000 ether);
+
+        // Create slates
+        address[] memory dummy = new address[](1);
+        dummy[0] = address(0);
+
+        address[] memory slate = new address[](1);
+        slate[0] = address(spell);
+
+        // Hat is address(0)
+        assertEq(chief.hat(), address(0));
+
+        // Alice votes for spell slate
+        vm.prank(alice);
+        bytes32 slateId = chief.vote(slate);
+
+        // Lift the hat
+        chief.lift(address(spell));
+        assertEq(chief.hat(), address(spell));
+
+        // Bob votes for dummy slate
+        vm.prank(bob);
+        chief.vote(dummy);
+
+        // Lift the hat
+        chief.lift(address(0));
+        assertEq(chief.hat(), address(0));
+
+        // Carol votes for spell slate
+        vm.prank(carol);
+        chief.vote(slateId);
+
+        // Lift the hat
+        chief.lift(address(spell));
+        assertEq(chief.hat(), address(spell));
+
+        // Cast the spell
+        //spell.cast();
     }
 
 }
